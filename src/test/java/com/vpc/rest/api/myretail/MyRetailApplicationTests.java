8d452f6c96c49e7d13c3158463cefd376fcc540a
Veera Prasad Chirukuri <veeraprasad512@gmail.com>
2021-07-12 08:44:15 -0400

    First Commit

---
+++ src/test/java/com/vpc/rest/api/myretail/MyRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpc.rest.api.myretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
